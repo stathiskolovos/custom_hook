11470378b902f16224afd00120efd7852e7619a0
Stathis Kolovos <stathiskolovos@MacBook.local>
2025-01-29 18:45:19 +0200

    fetch Yoga benefits also

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FetchYogaData'
 
 function App() {
   return (
- <>
- <FetchData/>
- </>
+    <>
+      <h1>Diet</h1>
+      <FetchData/>
+      <h1>Yoga Excercises</h1>
+      <FetchYogaData/>
+    </>
   )
 }
 
--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -14,9 +14,9 @@ const FetchData = () => {
             <li className='list_data'>
                 <h3>{e.name}</h3>
                 <p><strong>Importance: </strong>{e.importance}</p>
-                <p><strong>Benefits: </strong>{e.benefits6}</p>
+                <p><strong>Benefits: </strong>{e.benefits}</p>
                 <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
-                </li>
+              </li>
             </>
         ))}
       </ul>
